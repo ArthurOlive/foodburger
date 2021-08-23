fc7781822a18b7b414db66f8e8273e38ffed9981
ArthurOlive <arthuroliveira909.av@gmail.com>
2021-08-22 22:45:58 -0300

    feat. init

---
+++ android/app/src/main/kotlin/com/example/foodburger/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.foodburger
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Home());
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {'/': (context) => HomePage()},
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: Center(
+        child: SizedBox(
+          width: 400,
+          child: Card(),
+        ),
+      ),
+    );
+  }
+}
